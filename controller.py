434c99f1e1f0d3dd3f75bf3f37d1cbac8fb832bb
j4kim <jivkim@gmail.com>
2024-11-01 20:24:37 +0100

    map pin to video path

--- controller.py
+++ controller.py
@@ -1,12 +1,19 @@
 import RPi.GPIO as GPIO
 import time
 
+config = {
+    23: "IWITW variante.mp4",
+    24: "Jasmina.AVI",
+    25: "La Quizza.AVI",
+    8: "ouais ouais c'est la vie v2.mov"
+}
+
 GPIO.setmode(GPIO.BCM)
 
 def button_callback(pin):
-    print(pin)
+    print(config[pin])
 
-for pin in [23, 24]:
+for pin in config:
     GPIO.setup(pin, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
     GPIO.add_event_detect(pin, GPIO.RISING, callback=button_callback, bouncetime=200)
 
