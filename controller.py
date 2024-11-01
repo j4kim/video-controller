a699145d657b99ad90ec7f70340dd59c6988c307
j4kim <jivkim@gmail.com>
2024-11-01 22:33:25 +0100

    [not working] use rc mode of vlc to change video without killing and relaunching vlc

--- controller.py
+++ controller.py
@@ -9,21 +9,26 @@ config = {
     8: "ouais ouais c'est la vie v2.mov"
 }
 
+def getpath(pin):
+    return "/home/jo/samba/trucs/" + config[24]
+
 GPIO.setmode(GPIO.BCM)
 
-def play_video(pin):
-    subprocess.run(["pkill", "vlc"])
-    path = "/home/jo/samba/trucs/" + config[pin]
-    subprocess.Popen([
-        "vlc",
-        "--fullscreen",
-        "--no-video-title-show",
-        path
-    ])
+vlc = subprocess.Popen([
+    "vlc", "--fullscreen", "--no-video-title-show",
+    "--no-video-deco", "--no-embedded-video",
+    "--no-video-title-show", "--rc-fake-tty",
+    "--rc-unix", "/tmp/vlc.sock",
+    getpath(23)
+])
+
+def change_video(pin):
+    with open("/tmp/vlc.sock", "w") as vlc_socket:
+        vlc_socket.write(f"add {getpath(pin)}\n")
 
 for pin in config:
     GPIO.setup(pin, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
-    GPIO.add_event_detect(pin, GPIO.RISING, callback=play_video, bouncetime=200)
+    GPIO.add_event_detect(pin, GPIO.RISING, callback=change_video, bouncetime=200)
 
 try:
     while True:
@@ -34,4 +39,4 @@ except KeyboardInterrupt:
 
 finally:
     GPIO.cleanup()
-    subprocess.run(["pkill", "vlc"])
+    vlc.terminate()
