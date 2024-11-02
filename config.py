d1dce78854d76f71c90938ec41197d3472e49aa3
j4kim <jivkim@gmail.com>
2024-11-02 17:42:48 +0100

    move config in a module + remove socket if it exists

---
+++ config.py
@@ -0,0 +1,10 @@
+videos = {
+    23: "balle.AVI",
+    24: "papillon.mp4",
+    25: "pipou.mp4",
+    8: "poisson.mp4"
+}
+
+mpv_socket = "/tmp/mpv-socket"
+
+videos_dir = "/home/jo/samba/trucs/"
--- controller.py
+++ controller.py
@@ -3,22 +3,11 @@ import time
 import subprocess
 import socket
 import json
-
-config = {
-    23: "balle.AVI",
-    24: "papillon.mp4",
-    25: "pipou.mp4",
-    8: "poisson.mp4"
-}
-
-mpv_socket = "/tmp/mpv-socket"
-
-videos_dir = "/home/jo/samba/trucs/"
-
-GPIO.setmode(GPIO.BCM)
+import config
+import os
 
 def get_path(pin):
-    return videos_dir + config[pin]
+    return config.videos_dir + config.videos[pin]
 
 def run_mpv():
     return subprocess.Popen([
@@ -26,23 +15,28 @@ def run_mpv():
         "--fullscreen",
         "--no-osd-bar",
         "--no-border",
-        f"--input-ipc-server={mpv_socket}",
+        f"--input-ipc-server={config.mpv_socket}",
         "--idle"
     ])
 
 def send_mpv_command(command):
     with socket.socket(socket.AF_UNIX, socket.SOCK_STREAM) as sock:
-        sock.connect(mpv_socket)
+        sock.connect(config.mpv_socket)
         sock.sendall(json.dumps(command).encode("utf-8") + b"\n")
 
 def loadfile(pin):
     send_mpv_command({"command": ["loadfile", get_path(pin), "replace"] })
 
-for pin in config:
-    GPIO.setup(pin, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
-    GPIO.add_event_detect(pin, GPIO.RISING, callback=loadfile, bouncetime=200)
+if os.path.exists(config.mpv_socket):
+    os.remove(config.mpv_socket)
 
 mpv = run_mpv()
+
+GPIO.setmode(GPIO.BCM)
+
+for pin in config.videos:
+    GPIO.setup(pin, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+    GPIO.add_event_detect(pin, GPIO.RISING, callback=loadfile, bouncetime=200)
 
 try:
     while True:
